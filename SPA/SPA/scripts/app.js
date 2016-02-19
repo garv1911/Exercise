5ee0e3defe3d3d3f975801b38de94f754e8c7add
Gaurav Goyal <gauravgoyalxd@outlook.com>
2016-02-19 16:42:47 +0530

    SPA

---
+++ SPA/SPA/scripts/app.js
@@ -0,0 +1,26 @@
+﻿var module = angular.module("sampleApp", ['ngRoute']);
+
+module.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+                when('/route1', {
+                    title: 'Maruti',
+                    templateUrl: 'templates/create.html',
+                    controller: 'createController'
+                }).
+                when('/route2', {
+                    templateUrl: 'templates/delete.html',
+                    controller: 'deleteController'
+                }).
+                 when('/route3/:param', {
+                     templateUrl: 'templates/view.html',
+                     controller: 'viewController'
+                 }).
+                when('/route4', {
+                    templateUrl: 'templates/mypage.html'
+
+                }).
+                otherwise({
+                    redirectTo: '/route4'
+                });
+    }]);
---
+++ SPA/SPA/scripts/controllers.js
@@ -0,0 +1,26 @@
+﻿module.controller("createController", function ($scope, $route) {
+    $scope.cars = ['AUDI', 'BMW', 'MERCEDES'];
+    $scope.my = { favorite: 'AUDI' };
+    console.log($route.current.title);
+
+});
+module.controller("deleteController", function ($scope) {
+    $scope.clickCounter = 0;
+    $scope.check = function () {
+        $scope.clickCounter++;
+    };
+});
+module.controller("viewController", function ($scope, $filter, $routeParams) {
+    $scope.param = $routeParams.param;
+    $scope.cars = [
+       { carId: 001, carName: 'Santro' },
+       { carId: 002, carName: 'Swift Dzire' },
+       { carId: 003, carName: '120' },
+       { carId: 004, carName: 'Verna' },
+       { carId: 005, carName: 'City' }];
+    $scope.cars2 = $scope.cars;
+    $scope.$watch('search', function (val) {
+        $scope.cars = $filter('filter')($scope.cars2, val);
+    });
+});
+
